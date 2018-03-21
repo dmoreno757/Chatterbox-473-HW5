fac0c7bb1104d45d67027ade35c6901eb65348e1
david Moreno <dmoreno757@gmail.com>
2018-03-20 21:51:47 -0700

    Update 2

--- websockets-server.js
+++ websockets-server.js
@@ -1,24 +1,35 @@
-var WebSocket = require('ws');
+/*eslint no-undef: "error"*/
+/*eslint-env node*/
+var WebSocket = require("ws");
 var WebSocketServer = WebSocket.Server;
 var port = 3001;
 var ws = new WebSocketServer({
-    port: port
+  port: port
 });
+var topic;
 var messages = [];
-console.log('websockets server started');
+console.log("websockets server started");
 
-ws.on('connection', function (socket) {
-    console.log('client connection established');
+ws.on("connection", function (socket) {
+  console.log("client connection established");
+  if (topic) {
+    socket.send("***topic is" + " '" + topic + "'");
+  }
 
-    messages.forEach(function (msg) {
-        socket.send(msg);
+  messages.forEach(function (msg) {
+    socket.send(msg);
+  });
+
+  socket.on("message", function (data) {
+    console.log("message received: " + data);
+    messages.push(data);
+    ws.clients.forEach(function (clientSocket) {
+      if (data.substring(0, 6) == "/topic") {
+        clientSocket.send("*** Topic has changed to" + " '" + data.substring(7) + "'");
+        topic = data.substring(7);
+      } else {
+        clientSocket.send(data);
+      }
     });
-
-    socket.on('message', function (data) {
-        console.log('message received: ' + data);
-        messages.push(data);
-        ws.clients.forEach(function (clientSocket) {
-            clientSocket.send(data);
-        });
-    });
+  });
 });
